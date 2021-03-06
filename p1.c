99f94a48af773302a1e386c76619776064b8ff64
priyap803 <79666595+priyap803@users.noreply.github.com>
2021-03-06 16:59:30 -0500

    Add files via upload

--- p1.c
+++ p1.c
@@ -2,49 +2,83 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
-#include "strbuf.h"
+#include <assert.h>
+#include <unistd.h>
+#include "strbuf.c"
 
-int wrapout (char *str, int width, int strlen){ // stdout wrap text
-    char *base,*right_margin;
-    base = str;
-    while(*base){
-        if(strlen <= width){
-            puts(base);     /* display string */
-            return(0);      /* and leave */
-        }
-        right_margin = base+width;
-        while(!isspace(*right_margin)){
-            right_margin--;
-            if( right_margin == base){
-                right_margin += width;
-                while(!isspace(*right_margin)){
-                    if( *right_margin == '\0')
-                        break;
-                    right_margin++;
-                }
-            }
-        }
-        *right_margin = '\0';
-        puts(base);
-        strlen -= right_margin-base+1;      /* +1 for the space */
-        base = right_margin+1;
-    }
-    return 0;
+void print(strbuf_t w, int l){
+    char line[l];
+	for(int i = 0; i <l; i++){
+		line[i] = w.data[i];
+	}
+	 write(1, line, l);
 }
 
+int wrapout (strbuf_t input, unsigned int width){ // stdout wrap text
+int m = 0, space = width, exceed = 0, nl = 0, tab = 0, bl = 0;
+   while(m < input.used){
+       strbuf_t word;
+       int wordlength = 0;
+       while(isspace(input.data[m]) == 0){
+			sb_append(&word, input.data[m]);
+			wordlength++;
+            m++;
+		}
+        if (wordlength <= space){
+            space = space - wordlength;
+            print(word, wordlength);
+        }
+        else{ //if (wordlength > space)
+            write(1, "\n", 1);// put a newline
+            if(wordlength > width){
+                exceed++;
+                print(word, wordlength);//write the word one the newline then start a newline 
+                write(1, "\n", 1);
+                break;
+            }
+            space = width - wordlength;
+            print(word, wordlength);
+        }
+        while(isspace(input.data[m]) != 0){// dealing with space characters now
+		    if (input.data[m] == '\n'){
+                nl++;
+            }
+            else if (input.data[m] == '\t'){
+                tab++;
+            }
+            else if (input.data[m] == ' '){ 
+                bl++;
+            }
+            m++;
+		}
+        if (nl > 1){
+            for (int p = 0; p < (nl-1); p++){
+                write(1, "\n", 1);
+            }
+        }
+        if (tab > 0 || bl > 0){
+            if(space >= 1)
+                write(1, " ", 1);
+        }  
+        sb_destroy(&word); // done with the word
+   }
+return exceed;
+}
 
 int main(int argc, char **argv){
-    if(argc > 2 || argc < 1){ // too many arguments or too little arguments
+    if(argc > 3 || argc < 2){ // too many arguments or too little arguments
         return EXIT_FAILURE;
     }
-    int width = argv[0]; 
+    unsigned int width = atoi(argv[1]); 
     assert(width > 0);  
     if(argc == 1){ // no file or directory provided  
-        char *input = NULL; 
-        size_t length = 0;
-        ssize_t read;
-        read = getline(&input, &length, stdin);
-        int error = wrapout(input, width, length);
+        strbuf_t S;
+        char *input = malloc(sizeof(char)); 
+        int rval = read(0, input, sizeof(char));
+        sb_concat(&S, input);
+        free(input);
+        int error = wrapout(S, width);
+        sb_destroy(&S);
         if(error != 0) return EXIT_FAILURE; // a word had exceeded page length
     }
     
