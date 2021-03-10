391781c815ab35858023be5c3e868f7f0de8e84e
priyap803 <79666595+priyap803@users.noreply.github.com>
2021-03-10 15:51:30 -0500

    Add files via upload

--- Makefile.mk
+++ Makefile.mk
@@ -1,8 +1,8 @@
 projectwrap: projectwrap.o strbuf.o
-	gcc -g -std=c99 -Wvla -Wall -fsanitize=address,undefined -o p1 p1.o strbuf.o
+	gcc -g -std=c99 -Wvla -Wall -fsanitize=address,undefined -o projectwrap projectwrap.o strbuf.o
 
 projectwrap.o: projectwrap.c strbuf.h
-	gcc -c -g -std=c99 -Wvla -Wall -fsanitize=address,undefined p1.c
+	gcc -c -g -std=c99 -Wvla -Wall -fsanitize=address,undefined projectwrap.c
 
 strbuf.o: strbuf.c strbuf.h
 	gcc -c -g -std=c99 -Wvla -Wall -fsanitize=address,undefined strbuf.c
@@ -12,7 +12,7 @@ strbuf.o: strbuf.c strbuf.h
 dstrbuf.o: strbuf.c strbuf.h
 	gcc -c -g -std=c99 -Wvla -Wall -fsanitize=address,undefined -DDEBUG -o dstrbuf.o strbuf.c
 
-dp1: projectwrap.o dstrbuf.o
+dprojectwrap: projectwrap.o dstrbuf.o
 	gcc -g -std=c99 -Wvla -Wall -fsanitize=address,undefined -o projectwrap projectwrap.o dstrbuf.o
 
 clean:
