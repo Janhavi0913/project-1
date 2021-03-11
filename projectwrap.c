c337eb85d6d38b5056d5fd1ca369f56f5a9b762e
Janhavi0913 <61356171+Janhavi0913@users.noreply.github.com>
2021-03-11 13:00:09 -0500

    Update projectwrap.c

--- projectwrap.c
+++ projectwrap.c
@@ -80,17 +80,18 @@ sb_init(&word, 5);
    close(placefrom);
    return exceed;
 }
-int printfile(unsigned int width, char* filename, int fd)
+int printfile(unsigned int width, char* filename, int fd, char* dirname)
 {
     strbuf_t newfile;
     sb_init(&newfile, 6);
-    sb_concat(&newfile, "wrap.");
+    sb_concat(&newfile, dirname);
+    sb_concat(&newfile, "/wrap.");
     sb_concat(&newfile, filename);
-    printf("New name: %s\n", newfile.data);
-    int newfd = open(newfile.data, O_CREAT|O_WRONLY);
+    int newfd = open(newfile.data, O_WRONLY|O_CREAT, 600);
     int error = wrapout(width, fd, newfd);
     write(newfd, "\n", 1);
     sb_destroy(&newfile);
+    close(newfd);
     return error;
 }
 int StartsWith(const char *a, const char *b)
@@ -99,29 +100,34 @@ int StartsWith(const char *a, const char *b)
         return 1;
     return 0;
 }
-int wrapdir (unsigned int width, DIR* dir)
+int wrapdir (unsigned int width, DIR* dir, char* dirname)
 {
     struct dirent *cd;
     int willerror = 0;
     cd = readdir(dir);
+    strbuf_t file;
     while(cd != NULL)
     {
         char* f = cd->d_name;
-        if(cd->d_type == DT_REG)
+        int wr = startsWith(f, "wrap");
+        int pd = startsWith(f, ".");
+        if(wr == 0 && pd == 0)
         {
-            int wr = StartsWith(f, "wrap");
-            int pd = StartsWith(f, ".");
-            if(wr == 0 && pd == 0)
-            {
-                printf("%s\n", f);
-                int fd = open(f, O_RDONLY);
-                int error = printfile(width, f, fd);
+            sb_init(&file, 5);
+            sb_concat(&file, dirname);
+            sb_append(&file, '/');
+            sb_concat(&file, f);
+            int fd = open(file.data, O_RDONLY);
+            if(fd != -1){
+                int error = printfile(width, f, fd, dirname);
                 if(error != 0)
                     willerror = 1;
             }
+            file = newword(file);
         }
         cd = readdir(dir);
     }
+    sb_destroy(&file);
     closedir(dir);
     return willerror;
 }
@@ -151,7 +157,7 @@ int main(int argc, char **argv){
         }
         else
         {
-            int error = wrapdir(width, dir1);
+            int error = wrapdir(width, dir1, f);
             if(error != 0)
                 return EXIT_FAILURE;
         }
