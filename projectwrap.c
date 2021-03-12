dec8a6dd0e77bc4c85a00cf14eb6c4a7a4480b60
priyap803 <79666595+priyap803@users.noreply.github.com>
2021-03-11 20:21:08 -0500

    Update projectwrap.c

--- projectwrap.c
+++ projectwrap.c
@@ -7,6 +7,7 @@
 #include <dirent.h>
 #include <sys/types.h>
 #include <fcntl.h>
+#include <sys/stat.h>
 
 void print(strbuf_t w, int l, unsigned int placeto){ //can't we just do write(placeto, w.data, l)?
 char *line = malloc(sizeof(char) * l);
@@ -28,9 +29,9 @@ char *a = malloc(sizeof(char));
 int rval = read(placefrom, a, sizeof(char));
 strbuf_t word;
 sb_init(&word, width);
-while(isspace(a[0]) != 0 && rval == 1){
-  rval = read(placefrom, a, sizeof(char));
-}
+    while(isspace(a[0]) != 0 && rval == 1){
+        rval = read(placefrom, a, sizeof(char));
+    }
     while(rval == 1){       
        int wordlength = 0, nl = 0, tab = 0, bl = 0;
        while(isspace(a[0]) == 0 && rval == 1){
@@ -72,14 +73,13 @@ while(isspace(a[0]) != 0 && rval == 1){
             }
         rval = read(placefrom, a, sizeof(char));
         }
-        if(nl > 1){
-            for (int p = 0; p < (nl-1); p++){
-                write(placeto, "\n", 1);
-            }
+        if(nl >= 2){            
+            write(placeto, "\n", 1);
+            write(placeto, "\n", 1);
             space = width;
         }
         if(tab > 0 || bl > 0 || nl == 1){
-            if(space != width && space >= 1){
+            if(space != width && space >= 1 && rval != 0){
                 write(placeto, " ", 1);
                 space--;
             }
@@ -90,22 +90,20 @@ while(isspace(a[0]) != 0 && rval == 1){
    close(placefrom);
    return exceed;
 }
-int printfile(unsigned int width, char* filename, int fd, char* dirname)
-{
+int printfile(unsigned int width, char* filename, int fd, char* dirname){
     strbuf_t newfile;
     sb_init(&newfile, 6);
     sb_concat(&newfile, dirname);
     sb_concat(&newfile, "/wrap.");
     sb_concat(&newfile, filename);
-    int newfd = open(newfile.data, O_WRONLY|O_CREAT, 600);
+    int newfd = open(newfile.data, O_WRONLY|O_CREAT, 0600);
     int error = wrapout(width, fd, newfd);
     write(newfd, "\n", 1);
     sb_destroy(&newfile);
     close(newfd);
     return error;
 }
-int StartsWith(const char *a, const char *b)
-{
+int startsWith(const char *a, const char *b){
     if(strncmp(a, b, strlen(b)) == 0) 
         return 1;
     return 0;
@@ -117,40 +115,31 @@ int isdir(char *name) {
     closedir(dir1);
     return 1;
 }
-int isFileExistsStats(const char *path)
-{
+int isFileExistsStats(const char *path){
     struct stat stats;
-
     stat(path, &stats);
-
     // Check for file existence
     if (S_ISDIR(stats.st_mode))
         return 0;
-
     return 1;
 }
-int wrapdir (unsigned int width, DIR* dir, char* dirname)
-{
+int wrapdir (unsigned int width, DIR* dir, char* dirname){
     struct dirent *cd;
     int willerror = 0;
     cd = readdir(dir);
     strbuf_t file;
-    while(cd != NULL)
-    {
+    while(cd != NULL){
         char* f = cd->d_name;
         sb_init(&file, width);
         sb_concat(&file, dirname);
         sb_append(&file, '/');
         sb_concat(&file, f);
-        if(isFileExistsStats(file.data) == 1)
-        {
+        if(isFileExistsStats(file.data) == 1){
             int wr = startsWith(f, "wrap");
             int pd = startsWith(f, ".");
-            if(wr == 0 && pd == 0)
-            {
+            if(wr == 0 && pd == 0){
                 int fd = open(file.data, O_RDONLY);
-                if(fd != -1)
-                {
+                if(fd != -1){
                     int error = printfile(width, f, fd, dirname);
                     if(error != 0)
                         willerror = 1;
@@ -179,16 +168,14 @@ int main(int argc, char **argv){
     }
     else{
         char* f = argv[2];
-        if(isdir(f) == 0)
-        {
+        if(isdir(f) == 0){
             int fd = open(f, O_RDONLY);
             int error = wrapout(width, fd, 1);
             write(1, "\n", 1);
             if(error != 0)
                 return EXIT_FAILURE;
         }
-        else
-        {
+        else{
             DIR* dir1 = opendir(f);
             int error = wrapdir(width, dir1, f);
             if(error != 0)
